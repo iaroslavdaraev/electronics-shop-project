608acfcd44c521baea9e16017da0c6426e68c3d4
Iaroslav <yaroslav.da@mail.ru>
2024-01-22 22:11:56 +0300

    fix HW_13.2

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,4 +1,5 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from config import file_path
 from src.item import Item
 
 
@@ -15,15 +16,22 @@ def test_apply_discount():
     item1.apply_discount()
     assert item1.price == 450.0
 
+
 def test_name():
     item1 = Item('Смартфон', 500.0, 2)
     assert item1.name == 'Смартфон'
     item2 = Item('СуперСмартфон', 500.0, 2)
     assert item2.name == 'СуперСмартфон'
 
-# def test_instantiate_from_csv():
-#     assert isinstance(item_instances, list)
-#     item_instances = Item.instantiate_from_csv(file_path='src/items.csv')
+
+def test_instantiate_from_csv():
+    Item.instantiate_from_csv(file_path)
+    item1 = Item('Смартфон', 500.0, 2)
+    assert isinstance(Item.all, list)
+    assert item1.name == "Смартфон"
+    assert item1.price == 500
+    assert item1.quantity == 2
+
 
 def test_string_to_number():
     assert Item.string_to_number('7') == 7
